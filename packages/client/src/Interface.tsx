0d6d5b9e683792526567bf4577b8a2e9e105155e
Gryphon397 <jgfillman@gmail.com>
2026-04-01 22:48:14 -0400

    Fix PTT keybind activating when PTT disabled; fixes wrong sound cue (build 15)

--- packages/client/src/Interface.tsx
+++ packages/client/src/Interface.tsx
@@ -17,7 +17,7 @@ import { CircularProgress } from "@revolt/ui";
 
 import { Sidebar } from "./interface/Sidebar";
 
-const BUILD = 14;
+const BUILD = 15;
 console.info(`[stoat-dev] build ${BUILD}`);
 
 /**
