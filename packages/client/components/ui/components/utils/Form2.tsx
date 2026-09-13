b98667657ef9a8a620400b9fb8f624f38ef4f90e
Gryphon397 <jgfillman@gmail.com>
2026-09-13 19:14:55 -0400

    Upstream/U16: show file size validation errors in FormFileInput
    Cherry-pick of upstream 1eafe601 (#1518). Our FormFileInput predates the
    maxSize/hideErrors props upstream's base already had, so those are added
    here too. Also fixes the error list rendering the error *key* instead of
    the message.

--- packages/client/components/ui/components/utils/Form2.tsx
+++ packages/client/components/ui/components/utils/Form2.tsx
@@ -10,16 +10,25 @@ import {
   splitProps,
 } from "solid-js";
 
-import { Trans } from "@lingui-solid/solid/macro";
+import { Trans, useLingui } from "@lingui-solid/solid/macro";
 import { VirtualContainer } from "@minht11/solid-virtual-container";
 import { css } from "styled-system/css";
 import { styled } from "styled-system/jsx";
 
-import { Button, Checkbox, Radio2, Text, TextField } from "../design";
+import {
+  Button,
+  Checkbox,
+  Radio2,
+  Text,
+  TextField,
+  typography,
+} from "../design";
 import { TextEditor2 } from "../features/texteditor/TextEditor2";
-import { Row } from "../layout";
+import { Column, Row } from "../layout";
 
-import { FileInput } from "./files";
+import { useError } from "@revolt/i18n";
+
+import { FileInput, humanFileSize } from "./files";
 
 /**
  * Form wrapper for TextField
@@ -136,12 +145,22 @@ const FormFileInput = (
   props: {
     label?: string;
     control: IFormControl<File[] | string | null>;
+    maxSize?: number;
+    hideErrors?: boolean;
   } & Pick<
     ComponentProps<typeof FileInput>,
     "accept" | "imageAspect" | "imageRounded" | "imageJustify" | "allowRemoval"
   >,
 ) => {
-  const [local, remote] = splitProps(props, ["label", "control"]);
+  const [local, remote] = splitProps(props, [
+    "label",
+    "control",
+    "maxSize",
+    "hideErrors",
+  ]);
+
+  const err = useError();
+  const { t } = useLingui();
 
   return (
     <>
@@ -152,18 +171,51 @@ const FormFileInput = (
         {...remote}
         file={local.control.value}
         onFiles={(files) => {
-          // TODO: do validation of files here
+          if (
+            files &&
+            files.length > 0 &&
+            local.maxSize &&
+            files[0].size > local.maxSize
+          ) {
+            local.control.setErrors({
+              error: new Error(
+                t`File must be smaller than ${humanFileSize(local.maxSize)}`,
+              ),
+            });
+            local.control.markTouched(true);
+            return;
+          }
 
+          local.control.setErrors(null);
           local.control.setValue(files);
           local.control.markDirty(true);
         }}
         required={local.control.isRequired}
         disabled={local.control.isDisabled}
       />
-      <Show when={local.control.isTouched && !local.control.isValid}>
-        <For each={Object.keys(local.control.errors!)}>
-          {(errorMsg: string) => <small>{errorMsg}</small>}
-        </For>
+      <Show when={local.maxSize}>
+        <span class={typography({ class: "label", size: "small" })}>
+          (max. {humanFileSize(local.maxSize!)})
+        </span>
+      </Show>
+      <Show
+        when={
+          !local.hideErrors && local.control.isTouched && !local.control.isValid
+        }
+      >
+        <Column gap="sm">
+          <For each={Object.keys(local.control.errors!)}>
+            {(errorMsg: string) => (
+              <span
+                class={css(typography.raw({ class: "label", size: "small" }), {
+                  color: "var(--md-sys-color-error)",
+                })}
+              >
+                {err(local.control.errors![errorMsg])}
+              </span>
+            )}
+          </For>
+        </Column>
       </Show>
     </>
   );
